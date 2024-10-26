e348ed42089bc812f498ce4c168d37d79125d130
Ruhul Amin <ruhulamin.et15@gmail.com>
2024-10-26 10:10:53 +0600

    add default payment method

--- src/app/modules/PaymentMethod/paymentMethod.controller.ts
+++ src/app/modules/PaymentMethod/paymentMethod.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../../shared/catchAsync";
 import { paymentMethodService } from "./paymentMethod.service";
 import sendResponse from "../../../shared/sendResponse";
 
+//for login user
 const createPaymentMethod = catchAsync(async (req: any, res: Response) => {
   const result = await paymentMethodService.createPaymentMethodIntoDB(
     req.body,
@@ -17,6 +18,7 @@ const createPaymentMethod = catchAsync(async (req: any, res: Response) => {
   });
 });
 
+//for login user
 const getAllPaymentMethods = catchAsync(async (req: any, res: Response) => {
   const result = await paymentMethodService.getPaymentMethodsByUserIdFromDB(
     req.user.id
@@ -30,7 +32,36 @@ const getAllPaymentMethods = catchAsync(async (req: any, res: Response) => {
   });
 });
 
+//for admin
+const createMethod = catchAsync(async (req: any, res: Response) => {
+  const userId = req.user.id;
+  const result = await paymentMethodService.createMethod(req.body, userId);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 201,
+    message: "Payment Method Created Successfully",
+    data: result,
+  });
+});
+
+// get default payment methods
+const getDefaultPaymentMethods = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await paymentMethodService.getDefaultMethods();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: 200,
+      message: "Default Payment Methods Retrieved Successfully",
+      data: result,
+    });
+  }
+);
+
 export const paymentMethodController = {
   createPaymentMethod,
   getAllPaymentMethods,
+  createMethod,
+  getDefaultPaymentMethods,
 };
--- src/app/modules/PaymentMethod/paymentMethod.service.ts
+++ src/app/modules/PaymentMethod/paymentMethod.service.ts
@@ -1,6 +1,7 @@
 import prisma from "../../../shared/prisma";
 import ApiError from "../../errors/ApiErrors";
 
+//for login user
 const createPaymentMethodIntoDB = async (payload: any, userId: string) => {
   const result = await prisma.paymentMethod.create({
     data: {
@@ -12,6 +13,7 @@ const createPaymentMethodIntoDB = async (payload: any, userId: string) => {
   return result;
 };
 
+//for login user
 const getPaymentMethodsByUserIdFromDB = async (userId: string) => {
   const result = await prisma.paymentMethod.findMany({
     where: {
@@ -26,7 +28,35 @@ const getPaymentMethodsByUserIdFromDB = async (userId: string) => {
   return result;
 };
 
+//for admin
+const createMethod = async (payload: any, userId: string) => {
+  const result = await prisma.paymentMethod.create({
+    data: { ...payload, userId: userId },
+  });
+
+  return result;
+};
+
+// get default payment methods
+const getDefaultMethods = async () => {
+  const result = await prisma.paymentMethod.findMany({
+    where: {
+      user: {
+        role: "ADMIN",
+      },
+    },
+  });
+
+  if (result.length === 0) {
+    throw new ApiError(404, "Default payment methods not found");
+  }
+
+  return result;
+};
+
 export const paymentMethodService = {
   createPaymentMethodIntoDB,
   getPaymentMethodsByUserIdFromDB,
+  createMethod,
+  getDefaultMethods,
 };
