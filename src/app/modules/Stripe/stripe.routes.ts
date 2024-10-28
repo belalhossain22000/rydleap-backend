c56d6274027d18adb3ef1bf8d3c44a3110bafce5
Md Emdadullah <86929969+developeremdad@users.noreply.github.com>
2024-10-28 15:04:33 +0600

    stipe complete with full functionality

--- src/app/modules/Stripe/stripe.routes.ts
+++ src/app/modules/Stripe/stripe.routes.ts
@@ -1,9 +1,60 @@
 import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
 import { StripeController } from "./stripe.controller";
+import {
+  AuthorizedPaymentPayloadSchema,
+  capturedPaymentPayloadSchema,
+  refundPaymentPayloadSchema,
+  saveNewCardWithExistingCustomerPayloadSchema,
+  TStripeSaveWithCustomerInfoPayloadSchema,
+} from "./stripe.validation";
 
 const router = express.Router();
 
-// payment from owner to rider
+// create a new customer with card
+router.post(
+  "/save-card",
+  validateRequest(TStripeSaveWithCustomerInfoPayloadSchema),
+  StripeController.saveCardWithCustomerInfo
+);
+
+// Authorize the customer with the amount and send payment request
+router.post(
+  "/authorize-payment",
+  validateRequest(AuthorizedPaymentPayloadSchema),
+  StripeController.authorizedPaymentWithSaveCard
+);
+
+// Capture the payment request and deduct the amount
+router.post(
+  "/capture-payment",
+  validateRequest(capturedPaymentPayloadSchema),
+  StripeController.capturePaymentRequest
+);
+
+// Save new card to existing customer
+router.post(
+  "/save-new-card",
+  validateRequest(saveNewCardWithExistingCustomerPayloadSchema),
+  StripeController.saveNewCardWithExistingCustomer
+);
+
+// Get all save cards for customer
+router.get("/get-cards/:customerId", StripeController.getCustomerSavedCards);
+
+// Delete card from customer
+router.delete(
+  "/delete-card/:paymentMethodId",
+  StripeController.deleteCardFromCustomer
+);
+
+// Refund payment to customer
+router.post(
+  "/refund-payment",
+  validateRequest(refundPaymentPayloadSchema),
+  StripeController.refundPaymentToCustomer
+);
+
 router.post("/create-payment-intent", StripeController.createPaymentIntent);
 
 export const StripeRoutes = router;
